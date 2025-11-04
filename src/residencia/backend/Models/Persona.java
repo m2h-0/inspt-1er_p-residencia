dcbf543d5ce5db656a8ad08614b85e0853bd41ad
Matías Hayashi <hayashimario@yahoo.com>
2025-11-04 11:41:44 -0300

    Agrega constructor a Persona sin parámetro 'cod'

--- src/residencia/backend/Models/Persona.java
+++ src/residencia/backend/Models/Persona.java
@@ -15,6 +15,12 @@ public class Persona implements Almacenable {
         this.edad = edad;
     }
 
+    public Persona(int dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
     public int getDni() { return dni; }
     public String getNombre() { return nombre; }
     public int getEdad() { return edad; }
